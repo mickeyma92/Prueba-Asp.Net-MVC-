41b498d64d8b5d44a677672297d0fb86e8437601
mickeyma92 <microsoft1>
2017-04-15 18:34:19 -0500

    Full

---
+++ Demo Asp.Net EF2/Demo Asp.Net EF2/Controllers/ProductoController.cs
@@ -0,0 +1,124 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Demo_Asp.Net_EF2.Models;
+
+namespace Demo_Asp.Net_EF2.Controllers
+{
+    public class ProductoController : Controller
+    {
+        private Contexto db = new Contexto();
+
+        //
+        // GET: /Producto/
+
+        public ActionResult Index()
+        {
+            //return View(db.Productos.ToList());
+            return View();
+        }
+
+        //
+        // GET: /Producto/Details/5
+
+        public ActionResult Details(int id = 0)
+        {
+            Producto producto = db.Productos.Find(id);
+            if (producto == null)
+            {
+                return HttpNotFound();
+            }
+            return View(producto);
+        }
+
+        //
+        // GET: /Producto/Create
+
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        //
+        // POST: /Producto/Create
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(Producto producto)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Productos.Add(producto);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(producto);
+        }
+
+        //
+        // GET: /Producto/Edit/5
+
+        public ActionResult Edit(int id = 0)
+        {
+            Producto producto = db.Productos.Find(id);
+            if (producto == null)
+            {
+                return HttpNotFound();
+            }
+            return View(producto);
+        }
+
+        //
+        // POST: /Producto/Edit/5
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(Producto producto)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(producto).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(producto);
+        }
+
+        //
+        // GET: /Producto/Delete/5
+
+        public ActionResult Delete(int id = 0)
+        {
+            Producto producto = db.Productos.Find(id);
+            if (producto == null)
+            {
+                return HttpNotFound();
+            }
+            return View(producto);
+        }
+
+        //
+        // POST: /Producto/Delete/5
+
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Producto producto = db.Productos.Find(id);
+            db.Productos.Remove(producto);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            db.Dispose();
+            base.Dispose(disposing);
+        }
+    }
+}
